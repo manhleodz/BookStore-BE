42848538345ac4fa3a30c0662fc4f1d74d6c205b
manhleodz <doanducmanh100@gmail.com>
2023-11-18 14:54:25 +0700

    18/11

--- env.js
+++ env.js
@@ -1,4 +1,5 @@
+require('dotenv').config();
 module.exports = {
-    EMAIL: 'doanducmanh_t66@hus.edu.vn',
-    PASSWORD: 'ftsp nawf hrbq tvjd',
+    EMAIL: process.env.EMAIL,
+    PASSWORD: process.env.PASSWORD
 }
